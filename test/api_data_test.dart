10d90a7437e0556c8ccf7ea2854cec9b774dadba
Nikhil Singh <Singh.nikhil999@gmail.com>
2021-02-02 11:01:09 +0530

    Update api_data_test.dart

--- test/api_data_test.dart
+++ test/api_data_test.dart
@@ -50,15 +50,3 @@ main() {
   });
 }
 
-Future<Coin> fetchCoin(http.Client client) async {
-  final response =
-      await client.get('https://jsonplaceholder.typicode.com/posts/1');
-
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON.
-    return Coin.fromJson(jsonDecode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to load post');
-  }
-}
